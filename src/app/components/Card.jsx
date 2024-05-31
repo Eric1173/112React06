8dc8cd3835f33f17897278fb493afcc3715cca99
Yi Qun Lai <xshiftxstar@gmail.com>
2024-05-31 16:46:36 +0800

    create card components

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,10 @@
+/**
+ * @author Lai Yi Qun <B11117006@gemail.yuntech.edu.tw>
+ */
+export default function CustomCard({item}){
+    return(
+        <div class="border">
+            <div>{item.ScenicSpotName}</div>
+        </div>
+    )
+}
